d3aa7469dae11f89adfa45b568975342d7c211f5
Hlib Kavraskyi <h.kavraskyi@amoniac.eu>
2019-04-11 12:14:56 +0300

    Discard changes

--- src/scripts/build-native.js
+++ src/scripts/build-native.js
@@ -14,52 +14,6 @@ exec('tns --version', (err, stdout, stderr) => {
     if (tnsVersion >= 4) {
         console.log(`executing 'tns plugin build'`);
         exec('tns plugin build', (err, stdout, stderr) => {
-
-            console.log(`Cloning .js files`);
-            exec('cp -R ./*.js ../', (err, stdout, stderr) => {
-                if (err) {
-                    // node couldn't execute the command
-                    console.log(`${err}`);
-                    return;
-                }
-            })
-
-            console.log(`Cloning .d.ts files`);
-            exec('cp -R ./*.d.ts ../', (err, stdout, stderr) => {
-                if (err) {
-                    // node couldn't execute the command
-                    console.log(`${err}`);
-                    return;
-                }
-            })
-
-            console.log(`Cloning .json files`);
-            exec('cp -R package.json ../ && cp -R tsconfig.json ../', (err, stdout, stderr) => {
-                if (err) {
-                    // node couldn't execute the command
-                    console.log(`${err}`);
-                    return;
-                }
-            })
-
-            console.log(`Cloning typings files`);
-            exec('cp -R ./typings ../', (err, stdout, stderr) => {
-                if (err) {
-                    // node couldn't execute the command
-                    console.log(`${err}`);
-                    return;
-                }
-            })
-
-            console.log(`Cloning platforms files`);
-            exec('cp -R ./platforms ../', (err, stdout, stderr) => {
-                if (err) {
-                    // node couldn't execute the command
-                    console.log(`${err}`);
-                    return;
-                }
-            })
-
             if (err) {
                 // node couldn't execute the command
                 console.log(`${err}`);
